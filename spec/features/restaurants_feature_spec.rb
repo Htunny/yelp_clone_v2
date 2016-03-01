565f1f86aa60aaef559da9d8993c7322df55a624
Andrew <andy_htun@hotmail.com>
2016-03-01 14:04:30 +0000

    CRUD implemented

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -26,9 +26,44 @@ feature 'restaurants' do
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in "Name", with: 'Leon'
-      click_button 'Create restaurant'
+      click_button 'Create Restaurant'
       expect(page).to have_content 'Leon'
       expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'viewing restaurants' do
+    let!(:leon){Restaurant.create(name:'Leon')}
+
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'Leon'
+      expect(page).to have_content('Leon')
+      expect(current_path).to eq "/restaurants/#{leon.id}"
+    end
+  end
+
+  context 'editing restaurant' do
+    before { Restaurant.create name: 'Leon' }
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit Leon'
+      fill_in 'Name', with: 'LEON'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'LEON'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'deleting restaurants' do
+    before {Restaurant.create name: 'Leon'}
+
+    scenario 'removes a restaurant when the delete button is clicked' do
+      visit '/restaurants'
+      click_link 'Delete Leon'
+      expect(page).not_to have_content 'Leon'
+      expect(page).to have_content 'Restaurant deleted successfully'
     end
   end
 end
